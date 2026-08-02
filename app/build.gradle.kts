586a78f91650f488aee9ffcbeac0c9d86d8e571b
malklera <malklerulo@gmail.com>
2026-08-02 19:32:34 -0300

    fix: now it builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "ar.mimbi.Selfie"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "ar.mimbi.Selfie"
         minSdk = 31
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
